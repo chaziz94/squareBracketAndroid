1ee6b7507c4abc224d75c02358d53bda5b32173d
Chaziz <45898787+flickPF94@users.noreply.github.com>
2021-07-03 22:46:20 -0400

    i don't even know

--- app/src/main/java/com/gamerappa/squarebracket/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/gamerappa/squarebracket/ui/notifications/NotificationsViewModel.java
@@ -10,7 +10,7 @@ public class NotificationsViewModel extends ViewModel {
 
     public NotificationsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is profiles fragment, not finishec");
     }
 
     public LiveData<String> getText() {
